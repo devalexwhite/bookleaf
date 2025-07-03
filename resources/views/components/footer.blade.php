0bd59d3ca6ca63606284c45dd530432f4c2e1883
Alex White <alex.white@hey.com>
2025-07-03 09:35:32 -0400

    New home screenshot, updated footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4">
+    <aside class="grid-flow-col items-center">
+        <img src=" /images/logo-dark.svg" class="size-8" />
+        <p>An <a class="underline" href="https://thatalexguy.dev" target="_blank">Alex White</a> Product</p>
+    </aside>
+    <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end items-center">
+        <a href="https://www.buymeacoffee.com/devalexwhite" target="_blank" class="bg-white p-1 rounded">
+            <img src="/images/bmc-full-logo.svg" alt="Buy me a coffee" class="w-[90px]" />
+        </a>
+        <a href="https://github.com/devalexwhite/bookleaf" target="_blank">
+            <img src="/images/github.svg" class="size-6" />
+        </a>
+
+    </nav>
+</footer>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -20,20 +20,7 @@
         {{ $slot }}
     </main>
 
-    <footer class="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4">
-        <aside class="grid-flow-col items-center">
-            <img src=" /images/logo-dark.svg" class="size-8" />
-            <p>An <a class="underline" href="https://thatalexguy.dev" target="_blank">Alex White</a> Product</p>
-        </aside>
-        <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-            <a href="https://www.buymeacoffee.com/devalexwhite"><img
-                    src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=devalexwhite&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" /></a>
-            <a href="https://github.com/devalexwhite/bookleaf" target="_blank">
-                <img src="/images/github.svg" class="size-6" />
-            </a>
-
-        </nav>
-    </footer>
+    <x-footer />
 </body>
 
 </html>
